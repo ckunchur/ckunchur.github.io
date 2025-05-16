869f8fe5359011dc91091988ef3eae6ce99fae8b
Caitlin Kunchur <crkunchur@gmail.com>
2025-05-16 13:58:34 -0700

    content fixes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
+const { withContentlayer } = require('next-contentlayer');
+
 const nextConfig = {
+  output: 'export',
   images: {
     domains: ['zcsfxydhuacdmrtonkvi.supabase.co'],
+    unoptimized: true,
   },
-}
+  trailingSlash: true,
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  experimental: {
+    mdxRs: true,
+  },
+};
 
-module.exports = nextConfig 
+module.exports = withContentlayer(nextConfig); 
